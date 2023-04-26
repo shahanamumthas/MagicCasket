be816515ce1d54864fd99488ce4c4592549229f3
shahanamumthas <shahanabroto@gmail.com>
2023-04-26 16:52:43 +0530

    seventh commit

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({
   extended:true
 }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'./Public')))
 
 app.use('/admin',adminRouter)
 app.use('/product',productRouter)
