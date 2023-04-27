f32d3f07ca8ff8d4199f5702c81ea77eaeb85b8b
shahanamumthas <shahanabroto@gmail.com>
2023-04-27 15:06:21 +0530

    tenth commit

--- Middlewares/multer.js
+++ Middlewares/multer.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination:function(req,res,cb){
-        cb(null, "public");
+        cb(null, "Public");
     },
     filename: function (req, file, cb){
         const ext = file.mimetype.split("/")[1];
